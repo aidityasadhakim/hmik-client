d2dc70faff4e65c4d3ccc2b0f16829b5514c7c44
Leonfarhan <leonfarhan404@gmail.com>
2023-05-22 19:17:44 +0700

    Penambahan komponen navbar dan footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import HomeCarousel from "@/components/carousel/HomeCarousel";
+import FooterComponent from "@/components/footer/FooterComponent";
 import HomeContent from "@/components/home-content/HomeContent";
 import HomeSection2 from "@/components/home-content/HomeSection2";
 import Wave from "@/components/materials/Wave";
@@ -9,9 +10,10 @@ export default function Home() {
   return (
     <div>
       <HomeCarousel />
-      <HomeContent />
-      <Wave />
-      <HomeSection2 />
+      {/* <HomeContent /> */}
+      {/* <Wave /> */}
+      {/* <HomeSection2 /> */}
+      <FooterComponent />
     </div>
   );
 }
