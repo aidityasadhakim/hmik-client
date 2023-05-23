8b631a90028ea3f4d923bb3110b6c951481fa923
aidityas <aidityasadhakim250@gmail.com>
2023-05-23 14:30:08 +0700

    Change Naming Page for Build

--- src/app/members/[department]/page.jsx
+++ src/app/members/[department]/page.jsx
@@ -1,16 +1,14 @@
-// Photos from https://citizenofnowhe.re/lines-of-the-city
-
 "use client";
 
 import { useParams } from "next/navigation";
 
-const page = () => {
-  const params = useParams();
+const Page = () => {
+  const { department } = useParams();
   return (
     <div className="bg-[#1e40af] text-white">
-      <h1>Halaman department {params.department}</h1>
+      <h1>Halaman department {department}</h1>
     </div>
   );
 };
 
-export default page;
+export default Page;
