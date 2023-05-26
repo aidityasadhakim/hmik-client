1f1a0693d6ac64913958249343e339b2e7412fdd
aidityas <aidityasadhakim250@gmail.com>
2023-05-26 13:38:06 +0700

    Fix Favicon url

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -6,21 +6,24 @@ const Headers = () => {
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-touch-icon.png"
+        href="https://res.cloudinary.com/dhutys1vb/image/upload/v1685082988/hmik/favicon/apple-touch-icon_bzossu.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.png"
+        href="https://res.cloudinary.com/dhutys1vb/image/upload/v1685082987/hmik/favicon/favicon-32x32_fvadig.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.png"
+        href="https://res.cloudinary.com/dhutys1vb/image/upload/v1685082987/hmik/favicon/favicon-16x16_wnbhqs.png"
       />
-      <link rel="manifest" href="/site.webmanifest" />
+      <link
+        rel="manifest"
+        href="https://res.cloudinary.com/dhutys1vb/raw/upload/v1685083045/hmik/favicon/site_mzhiaf.webmanifest"
+      />
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#ffffff" />
       <title>HMIK | Homepage</title>
